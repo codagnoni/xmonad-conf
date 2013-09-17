d972d143278d54280f35925da1327855796ac529
joao.codagnoni <joao.codagnoni@datacom.ind.br>
2013-09-17 15:52:28 -0300

    changes to support Linux Mint Mate edition

---
+++ start-trayer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sleep 3
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand false --width 180 --widthtype pixel --transparent true --tint 0x222222 --alpha 0 --height 24 &
